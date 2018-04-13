002be2ad7b46492ab61253c938923f34b96f0f36
Julie K <julieskim160@gmail.com>
2018-04-13 11:34:10 -0700

    start store, reducer, action

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const RESTART_GAME = 'RESTART_GAME';
+export const restartGame = () => {
+    return {
+        type: RESTART_GAME
+
+    }
+}
+
+
---
+++ src/store.js
@@ -0,0 +1,9 @@
+import {createStore} from 'redux';
+// import {guessReducer} from './guessReducer';
+//store is built off reducers
+
+const store = createStore()
+
+
+
+export default store;
