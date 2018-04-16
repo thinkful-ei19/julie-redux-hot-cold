91584c53886445306a7784dbbaad75a771bd1de9
Julie K <julieskim160@gmail.com>
2018-04-16 11:23:30 -0700

    provider

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {Provider} from 'react-redux';
 import store from './store';
 import './reset.css';
 import './index.css';
-import {makeGuess, restartGame} from './action';
+// import {makeGuess, restartGame} from './action';
 
 import Game from './components/game';
 
-store.dispatch(makeGuess(13, 'Hot'));
-console.log(store.getState());
+// store.dispatch(makeGuess(13, 'Hot'));
+// console.log(store.getState());
 
-store.dispatch(restartGame());
-console.log(store.dispatch.getState());
 
 ReactDOM.render(
-  <Game />,
+  <Provider store={store}>
+    <Game />
+  </Provider>,
   document.getElementById('root')
 );
+
